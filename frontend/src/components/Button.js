2fc223d3a7a4c804aaacc717d18ef271ed3b1905
Guillermo Gonzalez Duque <guillermo.gonzalez.duque@gmail.com>
2021-09-12 12:54:22 +0200

    Small button Styling changes

--- frontend/src/components/Button.js
+++ frontend/src/components/Button.js
@@ -1,15 +1,13 @@
 import styled from 'styled-components/macro'
 
 export default styled.button`
-  padding: var(--size-m);
-  background: var(--accent);
-  border: 1px solid var(--accent);
-  color: var(--neutral-light);
+  display: block;
+  margin: auto;
+  padding: 10px;
+  border: 1px solid black;
+  color: black;
   font-size: 1em;
-  border-radius: var(--size-s);
-
-  :disabled {
-    border-color: var(--neutral-dark);
-    background: var(--neutral-dark);
-    color: var(--neutral-light);
-  }`
+  border-radius: 15px;
+  background: #b4e2ff;
+  margin-top: 20%;
+`
