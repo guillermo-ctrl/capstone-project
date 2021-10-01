ed27b3eaf9fc142944b26756c2fd655111794621
Guillermo Gonzalez Duque <guillermo.gonzalez.duque@gmail.com>
2021-10-01 09:40:53 +0200

    Add filter backend logic

--- backend/src/main/java/en/capstone/backend/repo/DocumentRepo.java
+++ backend/src/main/java/en/capstone/backend/repo/DocumentRepo.java
@@ -3,14 +3,18 @@ package en.capstone.backend.repo;
 import en.capstone.backend.model.DocumentEntity;
 import en.capstone.backend.model.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 import java.util.List;
 
-public interface DocumentRepo extends JpaRepository<DocumentEntity,Long> {
+public interface DocumentRepo extends JpaRepository<DocumentEntity,Long>, JpaSpecificationExecutor<DocumentEntity> {
 
     List<DocumentEntity> findAllByUserIs(UserEntity user);
 
     DocumentEntity findByImageIdAndUserIs(Long imageId, UserEntity user);
 
     DocumentEntity findByUrlAndUserIs(String url, UserEntity user);
+    List<DocumentEntity> findAllByCategoryAndUserIs(String category, UserEntity user);
+
+
 }
